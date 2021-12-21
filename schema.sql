e7c980df11462eb603e02f49bebd18c20a8b1097
Yurii Hanley <yuriihanley@Yuriis-MacBook-Pro.local>
2021-12-20 20:21:53 -0800

    Fixed Schema.

--- schema.sql
+++ schema.sql
@@ -17,12 +17,12 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(40) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -37,21 +37,14 @@ CREATE TABLE salaries (
 
 CREATE TABLE Dept_emp (
 	dept_no VARCHAR(40) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NUll,
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (dept_no)
-);
-
+	emp_no VARCHAR(40) NOT NUll,
+	from_date date NOT NULL,
+	to_date date NOT NULL
+	);
+	
 CREATE TABLE Titles(
 	emp_no INT NOT NULL,
-	title VARCHAR(40) NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NUll,
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	to_date DATE NOT NUll
 );
